00335c494516559a9b86ddd0b29fd21658eb3e84
aclark1997 <aclark7@unca.edu>
2023-02-20 14:44:45 -0500

    suggestions

--- js/main.js
+++ js/main.js
@@ -22,7 +22,36 @@ const showProfile = async (token) => {
         }
     })
     const data = await response.json();
+    
     console.log(data);
+    document.querySelector("#bannername").innerHTML = data.username;
+    document.querySelector("#avi").src = data.image_url;
+    document.querySelector("#username").innerHTML = `<p style="margin-right: 10px;"><b>${data.username}</b></p>`;
+}
+
+const showSuggestions = async (token) => {
+    const endpoint = `${rootURL}/api/suggestions`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    
+    data.forEach(suggie => {
+        console.log(suggie);
+        document.querySelector("#suggies").innerHTML += `
+        <div>
+        <img style="border-radius: 50%; width: 40px; height: 40px;" src="${suggie.image_url}" />
+        <div style="margin-top: -45px; margin-left: 3%;">
+          <p>${suggie.username}</p>
+          <p style="font-size: 12px; margin-top: -15px; color: rgb(131, 131, 131);">suggested for you</p>
+          <a class="linkbutton" href=""><p style="display: flex; margin-left: 150px; font-size: 14px; color: rgb(14, 122, 254); margin-top: -40px;">follow</p></a>
+        </div>
+      </div>
+        `;
+    });
 }
 
 const showPosts = async (token) => {
@@ -34,8 +63,11 @@ const initPage = async () => {
     // first log in (we will build on this after Spring Break):
     const token = await getAccessToken(rootURL, 'webdev', 'password');
 
-    showProfile();
+    // then use the access token provided to access data on the user's behalf
+    showStories(token);
+    showPosts(token);
+    showProfile(token);
+    showSuggestions(token);
 }
 
 initPage();
-
